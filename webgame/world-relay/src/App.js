bef5e77d1c90dcdfc0f6ad7bb9b7bcbd9676678f
be-student <presentsong@naver.com>
2021-12-07 11:12:36 +0900

    12

---
+++ webgame/world-relay/src/App.js
@@ -0,0 +1,7 @@
+import Template from "./component/Template.jsx";
+
+const App = () => {
+  return <Template>hello</Template>;
+};
+
+export default App;
---
+++ webgame/world-relay/src/component/Template.jsx
@@ -0,0 +1,10 @@
+import "./template.scss";
+
+const Template = () => {
+  return (
+    <div className="Template">
+      <div className="context"></div>
+    </div>
+  );
+};
+export default Template;
