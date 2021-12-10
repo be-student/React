a39a6398da987ea5e12d56a675ccda5c1131edf9
be-student <presentsong@naver.com>
2021-12-10 15:20:58 +0900

    16.2
    23

--- react book/context-tutorial/src/components/ColorBox.js
+++ react book/context-tutorial/src/components/ColorBox.js
@@ -3,45 +3,64 @@ import { ColorConsumer } from "../contexts/color";
 import { useContext } from "react";
 
 const ColorBox = () => {
+  // return (
+  //   //이런 형태로 Consumer사이에 중괄호 안에 함수 를 function as a child
+  //   //혹은 Render Props라고 함. children이 있을 자리에 JSX,문자열이 아닌
+  //   //함수를 만들어 내는 것.
+  //   <ColorConsumer>
+  //     {/*(value) => (
+  //       <div
+  //         style={{ width: "64px", height: "64px", background: value.color }}
+  //       />
+  //     )*/}
+  //     {/* 모르겠음
+  //     {function (value) {
+  //       return (
+  //         <div>
+  //           style={{ width: "64px", height: "64px", background: value.color }}
+  //         </div>
+  //       );
+  //     }} */}
+  //     {({ state }) => (
+  //       <div>
+  //         <div
+  //           style={{
+  //             width: "64px",
+  //             height: "64px",
+  //             background: state.color,
+  //           }}
+  //         />
+  //         <div
+  //           style={{
+  //             width: "32px",
+  //             height: "32px",
+  //             background: state.subcolor,
+  //           }}
+  //         />
+  //       </div>
+  //     )}
+  //   </ColorConsumer>
+  // );
+
+  //이 부분이 핵심. useContext라는 거를 통해 쉽게 접근 가능.
+  const { state } = useContext(ColorContext);
   return (
-    //이런 형태로 Consumer사이에 중괄호 안에 함수 를 function as a child
-    //혹은 Render Props라고 함. children이 있을 자리에 JSX,문자열이 아닌
-    //함수를 만들어 내는 것.
-
-    <ColorConsumer>
-      {/*(value) => (
-        <div
-          style={{ width: "64px", height: "64px", background: value.color }}
-        />
-      )*/}
-      {/* 모르겠음
-      {function (value) {
-        return (
-          <div>
-            style={{ width: "64px", height: "64px", background: value.color }}
-          </div>
-        );
-      }} */}
-
-      {({ state }) => (
-        <div>
-          <div
-            style={{
-              width: "64px",
-              height: "64px",
-              background: state.color,
-            }}
-          />
-          <div
-            style={{
-              width: "32px",
-              height: "32px",
-              background: state.subcolor,
-            }}
-          />
-        </div>
-      )}
-    </ColorConsumer>
+    <>
+      <div
+        style={{
+          width: "64px",
+          height: "64px",
+          background: state.color,
+        }}
+      />
+      <div
+        style={{
+          width: "32px",
+          height: "32px",
+          background: state.subcolor,
+        }}
+      />
+    </>
   );
 };
 
--- react book/context-tutorial/src/components/SelectColors.js
+++ react book/context-tutorial/src/components/SelectColors.js
@@ -1,37 +1,80 @@
 import { ColorConsumer } from "../contexts/color";
+import { Component } from "react";
+import ColorContext from "../contexts/color";
 
 const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
-const SelectColors = () => {
-  //ColorContext.Consumer인 ColorConsumer에서
-  //바로 다음줄 인자 이름은 내부 값과 같다.
-  return (
-    <div>
-      <h2>색상을 선택하세요.</h2>
-      <ColorConsumer>
-        {({ actions }) => (
-          <div style={{ display: "flex" }}>
-            {colors.map((color) => (
-              <div
-                key={color}
-                style={{
-                  background: color,
-                  width: "24px",
-                  height: "24px",
-                  cursor: "pointer",
-                }}
-                onClick={() => actions.setColor(color)}
-                onContextMenu={(e) => {
-                  e.preventDefault();
-                  actions.setSubcolor(color);
-                }}
-              />
-            ))}
-          </div>
-        )}
-      </ColorConsumer>
-      <hr />
-    </div>
-  );
-};
+// const SelectColors = () => {
+//   //ColorContext.Consumer인 ColorConsumer에서
+//   //바로 다음줄 인자 이름은 내부 값과 같다.
+//   return (
+//     <div>
+//       <h2>색상을 선택하세요.</h2>
+//       <ColorConsumer>
+//         {({ actions }) => (
+//           <div style={{ display: "flex" }}>
+//             {colors.map((color) => (
+//               <div
+//                 key={color}
+//                 style={{
+//                   background: color,
+//                   width: "24px",
+//                   height: "24px",
+//                   cursor: "pointer",
+//                 }}
+//                 onClick={() => actions.setColor(color)}
+//                 onContextMenu={(e) => {
+//                   e.preventDefault();
+//                   actions.setSubcolor(color);
+//                 }}
+//               />
+//             ))}
+//           </div>
+//         )}
+//       </ColorConsumer>
+//       <hr />
+//     </div>
+//   );
+// };
+class SelectColors extends Component {
+  //this.context조회시 현재context의 value를 가리킴.
+  //setColor호출 하고 싶다면 this.context.actions.setColor
+  //static contextType 정의시 클래스 매서드에서 Context에 둔 함수 호출 가능
+  //단점은 한 클래스 하나의 Context 뿐
+  //useContext를 쓰자. 함수형 쪽으로 선언.
+  static contextType = ColorContext;
+
+  handleSetColor = (color) => {
+    this.context.actions.setColor(color);
+  };
+  handleSetSubColor = (subcolor) => {
+    this.context.actions.setSubcolor(subcolor);
+  };
+  render() {
+    return (
+      <div>
+        <h2>색상을 선택하세요</h2>
+        <div style={{ display: "flex" }}>
+          {colors.map((color) => (
+            <div
+              key={color}
+              style={{
+                background: color,
+                width: "24px",
+                height: "24px",
+                cursor: "pointer",
+              }}
+              onClick={() => this.handleSetColor(color)}
+              onContextMenu={(e) => {
+                e.preventDefault();
+                this.handleSetSubColor(color);
+              }}
+            />
+          ))}
+        </div>
+        <hr />
+      </div>
+    );
+  }
+}
 export default SelectColors;
