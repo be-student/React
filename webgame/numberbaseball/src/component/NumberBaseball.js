b2c2074930336c8d38b1dd965060e10156a1ff2a
be-student <presentsong@naver.com>
2021-12-19 17:54:14 +0900

    ended
    1234

--- webgame/numberbaseball/src/component/NumberBaseball.js
+++ webgame/numberbaseball/src/component/NumberBaseball.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { isValidInputTimeValue } from "@testing-library/user-event/dist/utils";
+import { useState, useRef } from "react";
 const gennum = () => {
   let array = [];
   for (let i = 0; i < 4; i++) {
@@ -15,43 +16,88 @@ const gennum = () => {
 
 const NumberBaseball = () => {
   const [num, setNum] = useState(gennum);
-  const [val, setVal] = useState("");
+  const [input, setInput] = useState(""); //input
   const [tries, setTries] = useState(0);
   const [ball, setBall] = useState([]);
   const [result, setResult] = useState("");
+  const strikes = useRef(0);
+  const balls = useRef(0);
   const onChange = (e) => {
     const value = e.target.value.replace(/[^0-9]/gi, "");
-    setVal(value);
+    setInput(value);
   };
   const onSubmit = (e) => {
     e.preventDefault();
-    if (tries === 9) {
+
+    strikes.current = 0;
+    balls.current = 0;
+    if (num.join("") === input) {
+      setTries(0);
+      setResult("성공하셨습니다.");
+      setNum(gennum);
+    } else if (tries === 9) {
       setTries(0);
       setResult("10회 실패하셨습니다. 재시도합니다.");
       setNum(gennum);
+      setBall([]);
+      strikes.current = 0;
+      balls.current = 0;
     } else {
-      setBall((ba) => [...ba, val]);
+      calculateBS();
+      console.log(strikes.current, balls.current);
+      setBall((prev) => [
+        ...prev,
+        {
+          inputs: input,
+          strikess: strikes.current,
+          ballss: balls.current,
+        },
+      ]);
+
       setResult("");
       setTries(tries + 1);
     }
-    setVal("");
+    setInput("");
+  };
+  const calculateBS = () => {
+    for (let i = 0; i < 4; i++) {
+      const temp = parseInt(input[i]);
+      if (temp === num[i]) {
+        strikes.current += 1;
+      } else {
+        for (let j = 0; j < 4; j++) {
+          if (temp === num[j]) {
+            balls.current += 1;
+          }
+        }
+      }
+    }
   };
   return (
     <div>
       <div>
         <div>{num.toString()}</div>
         <form onSubmit={onSubmit}>
-          <input minLength="4" maxLength="4" onChange={onChange} value={val} />
+          <input
+            minLength="4"
+            maxLength="4"
+            onChange={onChange}
+            value={input}
+          />
         </form>
       </div>
       <h1>시도 : {tries}</h1>
       <h2>{result}</h2>
       <h2>시도한 내용</h2>
-      <ul>
+      <ol>
         {ball.map((b, index) => {
-          return <li key={index}>{b}</li>;
+          return (
+            <li key={index}>
+              {b.strikess}S {b.ballss}B {b.inputs}
+            </li>
+          );
         })}
-      </ul>
+      </ol>
     </div>
   );
 };
